d64e11e2bd9cceb04ee06b406dd267563886c18d
Raj131999 <poulrajdaniel13@gmail.com>
2026-08-15 17:23:58 +0530

    Update TongueTwistersScreen.kt

--- app/src/main/java/com/example/ui/screens/TongueTwistersScreen.kt
+++ app/src/main/java/com/example/ui/screens/TongueTwistersScreen.kt
@@ -1,6 +1,5 @@
 package com.example.ui.screens
 
-import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
